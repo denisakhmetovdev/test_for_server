11dd21fa8aa79514cf631d7534deb716048e6860
Denis-Akhmetov <denis.akhmetov.dev@gmail.com>
2022-11-10 17:20:08 +0600

    settings for deploy

--- src/test_for_server/settings.py
+++ src/test_for_server/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-1h8#3@^wr_%l19+3a4j=k$neqtz)-508pw9t80!eva&scm5)5*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
